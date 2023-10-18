1a3686b949921dcc80b6b8cb735acb3abd1cacdf
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-10-18 18:35:42 +0700

    ref: add debugbar, seeding user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,35 +11,17 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'cashier';
     protected $fillable = [
         'name',
         'email',
         'password',
         'role',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
 }
